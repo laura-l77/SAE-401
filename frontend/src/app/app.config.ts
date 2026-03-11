9d11ef3b6dcab6eb5923b19d30b7023302bf47a0
uisefr <admisefora@gmail.com>
2026-03-11 13:25:30 +0100

    problèmes régles

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,12 +1,15 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import 'zone.js';
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideClientHydration(),
+    provideCharts(withDefaultRegisterables())
   ]
 };
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,3 +1,4 @@
+import 'zone.js'; // <--  ligne ici pour js 
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
