5729d425e353e76d39fb042fa9a27e20eb0c617b
uisefr <admisefora@gmail.com>
2026-03-11 15:38:40 +0100

    changement home page

--- frontend/src/app/components/header/header.ts
+++ frontend/src/app/components/header/header.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router'; // <-- INDISPENSABLE
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink], // <-- AJOUTÉ ICI
   templateUrl: './header.html',
-  styleUrl: './header.css',
+  styleUrl: './header.css'
 })
 export class Header {}
