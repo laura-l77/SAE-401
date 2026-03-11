24edcf6e59bbe7e2b564788978ff2c89ba63c8ff
uisefr <admisefora@gmail.com>
2026-03-11 12:41:04 +0100

    réglage

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,23 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+// On adapte les chemins aux noms de tes fichiers (sans le ".component")
+import { Header } from './components/header/header';
+import { Sidebar } from './components/sidebar/sidebar';
+import { Dashboard } from './components/dashboard/dashboard';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet, 
+    HeaderComponent, 
+    SidebarComponent, 
+    DashboardComponent
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('frontend');
+  title = 'frontend';
 }
